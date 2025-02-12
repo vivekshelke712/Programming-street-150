5f13b0bb8405749bcae080271ca19495306d7d5b
vivekshelke712 <vivekshelke006@gmail.com>
2025-02-12 12:26:06 +0530

    Factorial

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println("Enter A Number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int fact = 1;
+        while (num != 1) {
+            fact = fact * num;
+            num--;
+        }
+        System.out.println("facroial is " + fact);
+
+    }
+}
